849e31473d0ff3bb021288ffc8f62f540fee94e6
Fahad <usergmfm@gmail.com>
2026-03-17 08:57:33 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shelfo/provider/navigation_provider.dart';
+import 'package:shelfo/routes/app_routes.dart';
+import 'package:shelfo/utils/theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NavigationProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Shelfo inventory',
+        theme: SFOAppTheme.light,
+        darkTheme: SFOAppTheme.dark,
+        themeMode: ThemeMode.light,
+        initialRoute: AppRoutes.splash,
+        routes: AppRoutes.routes,
+      ),
+    );
+  }
+}
