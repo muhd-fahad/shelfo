db3d053e7837d8cfdb942871336d36959703946a
Fahad <usergmfm@gmail.com>
2026-04-07 10:25:46 +0530

    splash screen updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,34 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shelfo/routes/app_routes.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _splash();
-  }
-
-  void _splash() async {
-    await Future.delayed(Duration(seconds: 3));
+  void _splash(BuildContext context) async {
+    await Future.delayed(const Duration(seconds: 3));
 
     // --- THIS IS THE FIX Async Gaps ---
-    if (!mounted) return;
+    if (!context.mounted) return;
     // -----------------------
 
-    // Now it is safe to use context
     Navigator.pushReplacementNamed(context, AppRoutes.businessInfo);
-    debugPrint("Splash");
   }
 
   @override
   Widget build(BuildContext context) {
+    _splash(context);
     return Scaffold(
       body: Center(
         child: Column(
@@ -41,37 +29,41 @@ class _SplashScreenState extends State<SplashScreen> {
                 crossAxisAlignment: .center,
                 mainAxisAlignment: .center,
                 children: [
-                  SvgPicture.asset("assets/logo/app_logo_Primary.svg",height: 48,fit: BoxFit.fitWidth,)
+                  SvgPicture.asset(
+                    "assets/logo/app_logo_Primary.svg",
+                    height: 48,
+                    fit: BoxFit.fitWidth,
+                  )
                 ],
               ),
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.only(top: 70),
+                padding: const EdgeInsets.only(top: 70),
                 decoration: ShapeDecoration(
                   color: const Color(0x4C16A34A),
                   shape: RoundedSuperellipseBorder(
-                    borderRadius: BorderRadius.vertical(
+                    borderRadius: const BorderRadius.vertical(
                       top: Radius.circular(30),
                     ),
                   ),
                 ),
                 child: Container(
-                  padding: EdgeInsets.only(top: 70),
+                  padding: const EdgeInsets.only(top: 70),
                   decoration: ShapeDecoration(
                     color: const Color(0x7F16A34A),
                     shape: RoundedSuperellipseBorder(
-                      borderRadius: BorderRadius.vertical(
+                      borderRadius: const BorderRadius.vertical(
                         top: Radius.circular(30),
                       ),
                     ),
                   ),
                   child: Container(
-                    padding: EdgeInsets.only(top: 70),
+                    padding: const EdgeInsets.only(top: 70),
                     decoration: ShapeDecoration(
                       color: const Color(0xB216A34A),
                       shape: RoundedSuperellipseBorder(
-                        borderRadius: BorderRadius.vertical(
+                        borderRadius: const BorderRadius.vertical(
                           top: Radius.circular(30),
                         ),
                       ),
