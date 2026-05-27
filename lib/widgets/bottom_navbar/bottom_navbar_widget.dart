1b6326dd5d60ba268ad7a5bb424c35b3d2ced0d2
Fahad <usergmfm@gmail.com>
2026-05-27 13:30:16 +0530

    UI update : bottomNavigationBar to NavigationDestination

--- lib/widgets/bottom_navbar/bottom_navbar_widget.dart
+++ lib/widgets/bottom_navbar/bottom_navbar_widget.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:shelfo/screens/inventory/inventory_screen.dart';
 import '../../provider/navigation_provider.dart';
 import '../../screens/home_screen.dart';
 import '../../screens/pos/pos_screen.dart';
 import '../../screens/sales_order_screen.dart';
-
 import 'package:shelfo/screens/settings/settings_screen.dart';
 
 class BottomNavbarWidget extends StatelessWidget {
@@ -16,43 +16,47 @@ class BottomNavbarWidget extends StatelessWidget {
     PosScreen(),
     SalesOrderScreen(),
     InventoryScreen(),
-    SettingsScreen()
+    SettingsScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     final navProvider = context.watch<NavigationProvider>();
     return Scaffold(
-      body: IndexedStack(index: navProvider.currentIndex, children: _pages),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navProvider.currentIndex,
-        onTap: (index) {
+      body: IndexedStack(
+        index: navProvider.currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navProvider.currentIndex,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        onDestinationSelected: (index) {
           context.read<NavigationProvider>().setIndex(index);
         },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_rounded),
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined, size: 24.r),
+            selectedIcon: Icon(Icons.home_rounded, size: 24.r),
             label: "Home",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.monitor_outlined),
-            activeIcon: Icon(Icons.monitor_rounded),
+          NavigationDestination(
+            icon: Icon(Icons.monitor_outlined, size: 24.r),
+            selectedIcon: Icon(Icons.monitor_rounded, size: 24.r),
             label: "POS",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_outlined),
-            activeIcon: Icon(Icons.shopping_cart_rounded),
+          NavigationDestination(
+            icon: Icon(Icons.shopping_cart_outlined, size: 24.r),
+            selectedIcon: Icon(Icons.shopping_cart_rounded, size: 24.r),
             label: "Sales",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inventory_2_outlined),
-            activeIcon: Icon(Icons.inventory_2_rounded),
+          NavigationDestination(
+            icon: Icon(Icons.inventory_2_outlined, size: 24.r),
+            selectedIcon: Icon(Icons.inventory_2_rounded, size: 24.r),
             label: "Stock",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_outlined),
-            activeIcon: Icon(Icons.settings_rounded),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined, size: 24.r),
+            selectedIcon: Icon(Icons.settings_rounded, size: 24.r),
             label: "Settings",
           ),
         ],
